c22f9b6cb26716627735fa8d45808e26a839404e
renatoathaydes <renatoathaydes@hotmail.com>
2019-09-07 09:49:28 +0200

    Added argument parser. Allow setting the log-level and to print help.

--- lib/src/_options.dart
+++ lib/src/_options.dart
@@ -1,26 +1,74 @@
+import 'dart:io';
+
+import 'package:args/args.dart';
+
+import 'helpers.dart';
+
 enum LogLevel { fine, debug, info, warn, error }
 
-LogLevel toLogLevel(String level) {
-  switch (level) {
-    case 'fine':
-      return LogLevel.fine;
-    case 'debug':
-      return LogLevel.debug;
-    case 'info':
-      return LogLevel.info;
-    case 'warn':
-      return LogLevel.warn;
-    case 'error':
-      return LogLevel.error;
-  }
-  return null;
+const logLevels = {
+  'fine': LogLevel.fine,
+  'debug': LogLevel.debug,
+  'info': LogLevel.info,
+  'warn': LogLevel.warn,
+  'error': LogLevel.error,
+};
+
+class _Options {
+  final LogLevel logLevel;
+
+  const _Options({this.logLevel = LogLevel.info});
 }
 
-class Options {
-  LogLevel logLevel = LogLevel.info;
-}
-
-final options = Options();
+_Options _options = const _Options();
 
 bool isLogEnabled(LogLevel logLevel) =>
-    options.logLevel.index <= logLevel.index;
+    _options.logLevel.index <= logLevel.index;
+
+LogLevel _parseLogLevel(String value) {
+  final logLevel = logLevels[value];
+  if (logLevel == null) {
+    throw StateError("Invalid log level: $value");
+  }
+  return logLevel;
+}
+
+List<String> parseOptionsAndGetTasks(List<String> args) {
+  final parser = ArgParser()
+    ..addOption(
+      'log-level',
+      abbr: 'l',
+      defaultsTo: 'info',
+      help: 'sets the log level',
+      allowed: logLevels.keys,
+    )
+    ..addFlag(
+      'help',
+      abbr: 'h',
+      negatable: false,
+      help: 'show this usage help message',
+    );
+  ArgResults parseResult;
+  try {
+    parseResult = parser.parse(args);
+  } on FormatException catch (e) {
+    return failBuild(reason: 'ERROR: ${e.message}\nUsage:\n${parser.usage}')
+        as List<String>;
+  }
+
+  if (parseResult.wasParsed('help')) {
+    print("Usage: dartle [<options>] [<tasks>]\n\n"
+        "Runs a Dartle build.\n"
+        "Tasks are declared in the dartle.dart file. If no task is given, the "
+        "default tasks are run.\n\nOptions:");
+    print(parser.usage);
+    exit(0);
+    return const [];
+  }
+
+  _options = _Options(
+    logLevel: _parseLogLevel(parseResult['log-level'].toString()),
+  );
+
+  return parseResult.rest;
+}
--- lib/src/core.dart
+++ lib/src/core.dart
@@ -8,21 +8,22 @@ import 'task.dart';
 
 Future<void> run(List<String> args,
     {@required List<Task> tasks, List<Task> defaultTasks}) async {
+  final taskNames = parseOptionsAndGetTasks(args);
   final taskMap = tasks.asMap().map((_, task) => MapEntry(task.name, task));
-  if (args.isEmpty) {
+  if (taskNames.isEmpty) {
     (defaultTasks ?? tasks).forEach(_runTask);
   } else {
-    await runWithArgs(args, taskMap);
+    await _runTasks(taskNames, taskMap);
   }
 }
 
-Future<void> runWithArgs(List<String> args, Map<String, Task> taskMap) async {
-  for (final arg in args) {
-    final task = taskMap[arg];
+Future<void> _runTasks(List<String> tasks, Map<String, Task> taskMap) async {
+  for (final taskName in tasks) {
+    final task = taskMap[taskName];
     if (task == null) return failBuild(reason: "Unknown task: ${task.name}");
   }
-  for (final arg in args) {
-    await _runTask(taskMap[arg]);
+  for (final taskName in tasks) {
+    await _runTask(taskMap[taskName]);
   }
 }
 
