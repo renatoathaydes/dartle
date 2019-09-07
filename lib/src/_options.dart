8a58505c3e47e69f93f57f0c4a5600b09833ca54
renatoathaydes <renatoathaydes@hotmail.com>
2019-09-07 08:28:39 +0200

    Created function to parse LogLevel.

--- lib/src/_options.dart
+++ lib/src/_options.dart
@@ -1,5 +1,21 @@
 enum LogLevel { fine, debug, info, warn, error }
 
+LogLevel toLogLevel(String level) {
+  switch (level) {
+    case 'fine':
+      return LogLevel.fine;
+    case 'debug':
+      return LogLevel.debug;
+    case 'info':
+      return LogLevel.info;
+    case 'warn':
+      return LogLevel.warn;
+    case 'error':
+      return LogLevel.error;
+  }
+  return null;
+}
+
 class Options {
   LogLevel logLevel = LogLevel.info;
 }
