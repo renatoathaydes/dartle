6ef5ab606176308a442cf56131da2c8f4acfdba7
renatoathaydes <renatoathaydes@hotmail.com>
2019-09-08 23:32:40 +0200

    Added example using task inputs/outputs.

--- example/dartle.dart
+++ example/dartle.dart
@@ -1,9 +1,21 @@
-import 'package:dartle/dartle.dart';
+import 'dart:convert';
+import 'dart:io';
 
-final allTasks = [Task(hello), Task(bye)];
+import 'package:dartle/dartle.dart';
+import 'package:dartle/src/io.dart';
+
+final allTasks = [
+  Task(hello),
+  Task(bye),
+  Task(encodeBase64,
+      description: 'Encodes input.txt in base64, writing to output.txt',
+      runCondition: FilesRunCondition(
+          inputs: FileCollection.file(File('input.txt')),
+          outputs: FileCollection.file(File('output.txt')))),
+];
 
 main(List<String> args) async =>
-    run(args, tasks: allTasks, defaultTasks: allTasks.sublist(0, 1));
+    run(args, tasks: allTasks, defaultTasks: [allTasks[0]]);
 
 hello() async {
   print("Hello!");
@@ -11,4 +23,9 @@ hello() async {
 
 bye() async {
   print("Bye!");
+}
+
+encodeBase64() async {
+  final input = await File('input.txt').readAsBytes();
+  await File('output.txt').writeAsString(base64.encode(input));
 }
