2d3c005a71a12115587cebe366cf6ea9c0241655
renatoathaydes <renatoathaydes@hotmail.com>
2019-11-07 20:50:12 +0100

    Several new tests based on running dartle build as a separate process. Wrote own methods to verify process output, so removed test_process dependency.

--- test/dartle_test.dart
+++ test/dartle_test.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 @TestOn('!browser')
 import 'package:dartle/dartle.dart';
 import 'package:test/test.dart';
-import 'package:test_process/test_process.dart';
+
+import 'test_utils.dart';
 
 helloTask() => null;
 
@@ -18,21 +21,93 @@ void main() {
     });
   });
   group('Task execution', () {
-    test('logs expected output', () async {
-      var proc =
-          await TestProcess.start('dart', ['example/dartle.dart', 'hello']);
-      await expectLater(proc.stdout,
-          emits(contains('Executing 1 task(s) out of 1 selected task(s)')));
-      await expectLater(proc.stdout, emits(contains("Running task 'hello'")));
-      await expectLater(proc.stdout, emits('Hello!'));
-      await proc.shouldExit(0);
+    final outputFile = File('example/output.txt');
 
-      proc = await TestProcess.start('dart', ['example/dartle.dart', 'bye']);
-      await expectLater(proc.stdout,
-          emits(contains('Executing 1 task(s) out of 1 selected task(s)')));
-      await expectLater(proc.stdout, emits(contains("Running task 'bye'")));
-      await expectLater(proc.stdout, emits('Bye!'));
-      await proc.shouldExit(0);
+    // create a snapshot so we can run the build quickly, several times
+    File exampleDartleBuild;
+
+    setUpAll(() async {
+      exampleDartleBuild =
+          (await createDartSnapshot(File('example/dartle.dart'))).absolute;
+    });
+    tearDownAll(() async {
+      await deleteAll(FileCollection([outputFile, exampleDartleBuild]));
+    });
+
+    Future<ProcessResult> runExampleDartBuild(List<String> args) async {
+      return startProcess(
+          runDartSnapshot(exampleDartleBuild,
+              args: args, workingDirectory: 'example'),
+          'example dart build');
+    }
+
+    test('logs expected output', () async {
+      var proc = await runExampleDartBuild(['hello']);
+      expect(proc.stdout[0],
+          contains('Executing 1 task(s) out of 1 selected task(s)'));
+      expect(proc.stdout[1], contains("Running task 'hello'"));
+      expect(proc.stdout[2], equals('Hello!'));
+      expect(proc.stdout[3], contains("Build succeeded"));
+      expect(proc.stdout.length, equals(4));
+      expect(proc.exitCode, equals(0));
+      expect(proc.stderr, isEmpty);
+
+      proc = await runExampleDartBuild(['bye']);
+      expect(proc.stdout[0],
+          contains('Executing 1 task(s) out of 1 selected task(s)'));
+      expect(proc.stdout[1], contains("Running task 'bye'"));
+      expect(proc.stdout[2], equals('Bye!'));
+      expect(proc.stdout[3], contains("Build succeeded"));
+      expect(proc.stdout.length, equals(4));
+      expect(proc.exitCode, equals(0));
+      expect(proc.stderr, isEmpty);
+    });
+
+    test('runs only tasks that are required, unless forced', () async {
+      // delete task output to make sure task runs
+      await ignoreExceptions(() async => await outputFile.delete());
+
+      var proc = await runExampleDartBuild(['encode']);
+      expect(proc.stdout[0],
+          contains('Executing 1 task(s) out of 1 selected task(s)'));
+      expect(proc.stdout[1], contains("Running task 'encodeBase64'"));
+      expect(proc.stdout[2], contains("Build succeeded"));
+      expect(proc.stdout.length, equals(3));
+      expect(proc.exitCode, equals(0));
+      expect(proc.stderr, isEmpty);
+
+      // verify that the task really ran
+      expect(await outputFile.exists(), isTrue);
+      expect(await outputFile.readAsString(), equals("SGVsbG8gRGFydGxlIQ=="));
+
+      // now the output exists, it should not run again
+      proc = await runExampleDartBuild(['encode']);
+      expect(proc.stdout[0],
+          contains('Executing 0 task(s) out of 1 selected task(s)'));
+      expect(proc.stdout[1], contains("Build succeeded"));
+      expect(proc.stdout.length, equals(2));
+      expect(proc.exitCode, equals(0));
+      expect(proc.stderr, isEmpty);
+
+      // when we force the task to run, it must run again
+      proc = await runExampleDartBuild(['encode', '-f']);
+      expect(proc.stdout[0],
+          contains('Executing 1 task(s) out of 1 selected task(s)'));
+      expect(proc.stdout[1], contains("Running task 'encodeBase64'"));
+      expect(proc.stdout[2], contains("Build succeeded"));
+      expect(proc.stdout.length, equals(3));
+      expect(proc.exitCode, equals(0));
+      expect(proc.stderr, isEmpty);
+    });
+
+    test('errors if task does not exist', () async {
+      // delete task output to make sure task runs
+      await ignoreExceptions(() async => await outputFile.delete());
+
+      var proc = await runExampleDartBuild(['foo']);
+      expect(proc.stdout[0], contains('ERROR - Unknown task'));
+      expect(proc.stdout[1], contains('Build failed'));
+      expect(proc.exitCode, equals(1));
     });
   });
 }
--- test/test_utils.dart
+++ test/test_utils.dart
@@ -1,9 +1,30 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:dartle/dartle.dart';
 import 'package:file/file.dart';
 
 FutureOr<R> withFileSystem<R>(FileSystem fs, FutureOr<R> Function() action) {
   return IOOverrides.runZoned(action,
       createDirectory: fs.directory, createFile: fs.file);
+}
+
+Future<ProcessResult> startProcess(Future<Process> process, String name) async {
+  final stdout = StdStreamConsumer(keepLines: true);
+  final stderr = StdStreamConsumer(keepLines: true);
+  final code = await exec(process,
+      name: name, onStdoutLine: stdout, onStderrLine: stderr);
+  return ProcessResult(stdout, stderr, code);
+}
+
+class ProcessResult {
+  final StdStreamConsumer _stdout;
+  final StdStreamConsumer _stderr;
+  final int exitCode;
+
+  ProcessResult(this._stdout, this._stderr, this.exitCode);
+
+  List<String> get stdout => _stdout.lines;
+
+  List<String> get stderr => _stderr.lines;
 }
