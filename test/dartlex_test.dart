e951592800f7912d1997152614aeacd3351817f6
Renato Athaydes <renato@athaydes.com>
2021-06-09 22:11:03 +0200

    Test that dartlex won't re-compile script without need.

--- test/dartlex_test.dart
+++ test/dartlex_test.dart
@@ -75,6 +75,27 @@ void main() {
       expect(result2.stdout, hasLength(equals(6)));
       expect(result2.stdout[4], equals('bye'));
       expect(result2.stderr, equals([]));
+
+      // make sure it doesn't re-compile if there's no changes...
+
+      // some OS's like to keep file change time resolution in the seconds
+      await Future.delayed(Duration(seconds: 1));
+
+      final result3 = await _runDartle(const ['hello'], projectDir);
+
+      expect(result3.exitCode, equals(0), reason: result3.stdout.toString());
+      expect(result3.stdout, hasLength(equals(4)));
+      expect(
+          result3.stdout[0],
+          matches(
+              '$infoLogPrefixRegex Executing 1 task out of a total of 1 task.*'));
+      expect(
+          result3.stdout[1],
+          matches(
+              '$logDateRegex - dartle\\[main \\d+\\] - INFO - Running task \'hello\''));
+      expect(result3.stdout[2], matches('bye'));
+      expect(result3.stdout[3], matches('✔ Build succeeded in .*'));
+      expect(result3.stderr, equals([]));
     });
   });
 }
