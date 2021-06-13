7c621c6d7cdff3a6c734bc04dffe77b296f87197
Renato Athaydes <renato@athaydes.com>
2021-06-13 19:20:52 +0200

    Removed runInShell from exec. It breaks on Windows.

--- lib/src/exec.dart
+++ lib/src/exec.dart
@@ -39,7 +39,7 @@ Future<Process> runDartExe(File dartExec,
             'not exist: ${dartExec.path}');
   }
   final proc = Process.start(dartExec.absolute.path, args,
-      workingDirectory: workingDirectory, runInShell: true);
+      workingDirectory: workingDirectory);
 
   logger.fine('Running compiled Dartle build: ${dartExec.path}');
 
