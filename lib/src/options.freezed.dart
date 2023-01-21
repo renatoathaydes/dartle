f6dc81ceba57d4e02ffcd0e0a8aee6c07d316cdd
Renato Athaydes <renato@athaydes.com>
2023-01-21 20:19:26 +0100

    Updating all libraries.
    Generated code updates.

--- lib/src/options.freezed.dart
+++ lib/src/options.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'options.dart';
 
@@ -286,6 +286,7 @@ class _$_Options extends _Options {
   @override
   @JsonKey()
   List<String> get tasksInvocation {
+    if (_tasksInvocation is EqualUnmodifiableListView) return _tasksInvocation;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_tasksInvocation);
   }
