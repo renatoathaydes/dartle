fa3616c0cd3a1d81f4ecd6dc394d61aa229e2a9e
Renato Athaydes <renato@athaydes.com>
2026-05-14 17:08:46 +0200

    Moved cleanWorkingDirs task to test dependency.

--- dartle.dart
+++ dartle.dart
@@ -21,7 +21,7 @@ void main(List<String> args) {
   ).task;
   final cleanupTask = createCleanWorkingDirsTask();
 
-  checkImportsTask.dependsOn(const {'cleanWorkingDirs'});
+  dartleDart.test.dependsOn(const {'cleanWorkingDirs'});
   distributionTask.dependsOn({dartleDart.compileExe.name});
   dartleDart.analyzeCode.dependsOn(const {
     'generateDartSources',
